54b116411ce7c1315daf5520a8ebb846fde50721
Kohei kiyota <jza80jza@gmail.com>
2021-09-14 21:55:46 +0900

    Initial Commit

---
+++ BleTest20210001/BleTest20210001App.swift
@@ -0,0 +1,17 @@
+//
+//  BleTest20210001App.swift
+//  BleTest20210001
+//
+//  Created by user on 2021/09/14.
+//
+
+import SwiftUI
+
+@main
+struct BleTest20210001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
